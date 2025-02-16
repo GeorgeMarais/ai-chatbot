5ac05c8ab85e627b9ca1cf8ef5cc926744905615
George <maraisgeorge39@gmail.com>
2025-02-16 16:21:17 +0000

    Add loading spinner to chat component and replace Image with img tag in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Image from "next/image";
 import React from "react";
 
 export default function Document() {
@@ -13,8 +12,8 @@ export default function Document() {
       </Head>
       <body className="antialiased h-full max-w-[2000px]">
         <header className="flex justify-left items-center py-5 px-12">
-          <Image className="w-auto h-7 relative object-contain block dark:hidden" src="https://mintlify.s3.us-west-1.amazonaws.com/maihem-29/logo/MAIHEM_Bittersweet_Full_Logo.svg" alt="light logo" />
-          <Image className="w-auto h-7 relative object-contain hidden dark:block" src="https://mintlify.s3.us-west-1.amazonaws.com/maihem-29/logo/MAIHEM_Bittersweet_Full_Logo.svg" alt="dark logo" />
+          <img className="w-auto h-7 relative object-contain block dark:hidden" src="https://mintlify.s3.us-west-1.amazonaws.com/maihem-29/logo/MAIHEM_Bittersweet_Full_Logo.svg" alt="light logo" />
+          <img className="w-auto h-7 relative object-contain hidden dark:block" src="https://mintlify.s3.us-west-1.amazonaws.com/maihem-29/logo/MAIHEM_Bittersweet_Full_Logo.svg" alt="dark logo" />
         </header>
         <Main />
         <NextScript />
